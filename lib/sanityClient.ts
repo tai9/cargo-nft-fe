d9c56eb885a1ac1901dca2e7cde336f71bf36a70
Tai <tantai.it95@gmail.com>
2022-06-11 20:59:02 +0700

    move sanity env variables

--- lib/sanityClient.ts
+++ lib/sanityClient.ts
@@ -1,10 +1,9 @@
 import sanityClient from '@sanity/client'
 
 export const client = sanityClient({
-  projectId: '4s3a37gw',
-  dataset: 'production',
-  apiVersion: '2021-03-25',
-  token:
-    'skyi1Kh4TBzzuKsSPKn4DsSSVNOWMljov0PpGmTOayRPnGGBfNfyzThh7NYVA1oiJ807P3RIVH8KE23t7D6HoUeXuptrFYi3dc98Plr2IZyH40qOZ16wAHzYVdJzoGgC7UvFfAciyxtysA8hXkpOsWIXfPN9eK8plm6pC7Q3M2eyVn4ZgDLH',
-  useCdn: false,
+  dataset: process.env.NEXT_PUBLIC_SANITY_DATASET || '',
+  projectId: process.env.NEXT_PUBLIC_SANITY_PROJECT_ID || '',
+  useCdn: process.env.NODE_ENV === 'production',
+  apiVersion: process.env.NEXT_PUBLIC_SANITY_API_VERSION,
+  token: process.env.NEXT_PUBLIC_SANITY_TOKEN || '',
 })
